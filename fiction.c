bd8603d9f51f080858cf2cb2d29d6a03afa4c319
Hugoni <gehomia@gmail.com>
2024-06-14 12:13:08 +0800

    Modify a little of the push just now

--- fiction.c
+++ fiction.c
@@ -2,6 +2,9 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <string.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_ttf.h>
 #include "toml.h"
 #include "fiction.h"
 #include "constants.h"
